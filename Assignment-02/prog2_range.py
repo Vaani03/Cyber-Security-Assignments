0aea7e55263b817c283c5f6e27a8920fd215fb13
Varnika Chauhan <https://github.com/Vaani03/Cyber-Security-Assignments.git>
2020-09-02 10:09:15 +0530

    prog 2

---
+++ Assignment-02/prog2_range.py
@@ -0,0 +1,7 @@
+n=int(input("enter your number"))
+a=int(input("input lower range"))
+b=int(input("input higher range"))
+if(a<=n and n<=b):
+     print("number is in given range")
+else:
+     print("number is not in range")
